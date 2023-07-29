076f6da1a2ca50ba93aa4aff29748aaf1402d7b8
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2023-07-29 18:58:15 +0200

    comments add

--- js/task-02.js
+++ js/task-02.js
@@ -6,14 +6,20 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+// znajduje listę
 const igredientsElem = document.querySelector('#ingredients');
 
+// tworzę stałą w której będę "zbierać" dane do tego,aby potem zrobić "append" do html
 const ingredientsArray = [];
 
 ingredients.forEach(ingredient=>{
+  // iteruję po liście, dodając "li" kazdemu ingredients
 const ingredientLi = document.createElement('li');
+// nadaję textContent, bez tego bęfdzie tylko <li> 
 ingredientLi.textContent = ingredient
+// dodaję klasy
 igredientsElem.classList.add('item')
+// robię "push" do listy którą potem zrobię "append"
 ingredientsArray.push(ingredientLi)
 
 })
