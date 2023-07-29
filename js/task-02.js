ac708435a9d1b68cd098d3f2002ac42102e69deb
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2023-07-29 17:57:57 +0200

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const igredientsElem = document.querySelector('#ingredients');
+
+const ingredientsArray = [];
+
+ingredients.forEach(ingredient=>{
+const ingredientLi = document.createElement('li');
+ingredientLi.textContent = ingredient
+igredientsElem.classList.add('item')
+ingredientsArray.push(ingredientLi)
+
+})
+igredientsElem.append(...ingredientsArray);
