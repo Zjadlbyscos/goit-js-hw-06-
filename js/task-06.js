6b38fb7c60c36bbead572720be48b83294a8d814
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2023-07-31 12:16:49 +0200

    task 06 done on basic way

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,15 @@
-const input = document.querySelector('#validation-input');
+const input = document.querySelector("#validation-input");
+
+function validateByLength() {
+  const inputLength = this.value.length;
+  const requiredLength = this.getAttribute("data-length");
+
+  if (inputLength == requiredLength) {
+    this.classList.add("valid");
+    this.classList.remove("invalid");
+  } else {
+    this.classList.add("invalid");
+    this.classList.remove("valid");
+  }
+}
+input.addEventListener("blur", validateByLength);
