9ec41b77e0fb41860e6f5aa5b72e25c5b0c0d644
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2023-08-01 21:09:31 +0200

    comments to task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,9 @@
 const input = document.querySelector("#validation-input");
-
+// wazne, this.
 function validateByLength() {
   const inputLength = this.value.length;
   const requiredLength = this.getAttribute("data-length");
-
+// tu są tylko dwa znaki =
   if (inputLength == requiredLength) {
     this.classList.add("valid");
     this.classList.remove("invalid");
@@ -13,3 +13,5 @@ function validateByLength() {
   }
 }
 input.addEventListener("blur", validateByLength);
+
+
