d4aeedc23ab10c0b0ac1bfe8941b094611970b93
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2023-08-02 08:13:17 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const loginFrm = document.querySelector(".login-form");
 
+const handleSubmit = (e) => {
+// zapobiega odświezaniu i domyslnym przeglądarki.
+  e.preventDefault(); 
+// destrukturyzacja dla przejrzystosci 
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be filled");
+  }
+
+  const frmUser = {
+    email: email.value,
+    password: password.value,
+  };
+// wyczyszczenie form po submit
+  console.log(frmUser);
+  loginFrm.reset();
+};
+
+loginFrm.addEventListener("submit", handleSubmit);
+// zainicjowanie działania 
